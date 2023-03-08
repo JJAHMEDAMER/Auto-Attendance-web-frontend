9509a4a8f4328ca07886609ac79dcb7f4f48f511
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-03-09 00:21:30 +0200

    getStarted navigate to appropriate route

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -8,9 +8,21 @@ import { myFetchGet } from "../utils/myFetch";
 // User Context
 import UserContext from "../utils/UserContext"
 
+// Router
+import { useNavigate } from "react-router-dom";
+
 export const Home = () => {
 
     const {token, setToken} = useContext(UserContext)
+    const navigate = useNavigate()
+
+    const handleGetStarted = ()=>{
+        if(token){
+            navigate("/")
+        }else{
+            navigate("/login")
+        }
+    }
 
     useEffect(() => {
         myFetchGet('', token)
@@ -23,7 +35,7 @@ export const Home = () => {
                 <div className="flex flex-col align-middle justify-center gap-6 w-1/2 text-white">
                     <h1 className="font-bold text-5xl">Auto Attendance System</h1>
                     <p className="">This is main Way to register for the Auto Attendance System</p>
-                    <button className="font-semibold text-lg p-2 bg-pink-600 rounded-full">Get Started!</button>
+                    <button onClick={handleGetStarted} className="font-semibold text-lg p-2 bg-pink-600 rounded-full">Get Started!</button>
                 </div>
                 <img src={IMG} className="w-1/2" alt="Smart Camps Illustration" />
             </div>
