e98f3c5c5aff90a85881846ddd1e87acc4a8f515
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-04-10 03:13:27 +0200

    make dashboard protected route

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,6 +1,23 @@
+// React
+import {useContext} from "react"
+
+// Comp
 import { Nav } from "../comp"
 
+// Router
+import { Navigate } from "react-router-dom";
+
+//context
+import UserContext from '../utils/UserContext'
+
+
 export const Dashboard = () => {
+    const { token, setToken } = useContext(UserContext)
+
+    if (token === null) {
+        return <Navigate to="/login" replace />;
+    }
+
     return (
         <div className="h-screen flex flex-col items-center justify-center">
             <Nav />
