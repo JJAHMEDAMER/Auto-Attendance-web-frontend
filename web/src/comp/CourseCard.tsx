42d4196024bfd959fbb0e897dc56428a0acc75b2
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-04-11 01:01:32 +0200

    attach data to button

---
+++ web/src/comp/CourseCard.tsx
@@ -0,0 +1,27 @@
+type courseCardPropType = {
+    name: String
+    code: String
+    location: String
+    handleClick: () => {}
+}
+
+
+export const CourseCard = ({ name, code, location, handleClick }: courseCardPropType) => {
+    return (
+        <div className="flex justify-between items-center w-2/3 max-w-2xl px-3 py-2 mb-1 border border-slate-900 rounded-lg bg-slate-800 mx-auto">
+            <div>
+                <h2 className="text-xs">{code}</h2>
+                <h1 className="text-xl">{name}</h1>
+                <p className="text-pink-600 font-bold text-sm">Location: <span className="uppercase">{location}</span></p>
+            </div>
+            <button
+                data-code={code}
+                data-name={name}
+                onClick={handleClick}
+                className="rounded-full bg-slate-900 hover:bg-pink-600 px-2 py-1"
+            >
+                Register
+            </button>
+        </div>
+    )
+}
