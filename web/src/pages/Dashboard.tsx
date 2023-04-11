edbf0dca03af3f7fc2aff52da127d2adcb98db1d
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-04-11 06:04:53 +0200

    use CourseCard Component with fake data

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,8 +1,8 @@
 // React
-import {useContext} from "react"
+import { useContext } from "react"
 
 // Comp
-import { Nav } from "../comp"
+import { Nav, CourseCard } from "../comp"
 
 // Router
 import { Navigate } from "react-router-dom";
@@ -14,6 +14,22 @@ import UserContext from '../utils/UserContext'
 export const Dashboard = () => {
     const { token, setToken } = useContext(UserContext)
 
+    const courses = [
+        { name: "Graduation project", code: "ASU111", location: "hall 3" },
+        { name: "Microwave", code: "ASU112", location: "hall A" }
+    ]
+
+    const registeredCourses = [
+        { name: "STC", code: "ASU113", location: "911" },
+        { name: "Electronics", code: "ASU114", location: "254" }
+    ]
+
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        console.log((e.target as HTMLInputElement).getAttribute("data-code"))
+        console.log((e.target as HTMLInputElement).dataset.code)
+    }
+
+
     if (token === null) {
         return <Navigate to="/login" replace />;
     }
@@ -21,8 +37,37 @@ export const Dashboard = () => {
     return (
         <div className="h-screen flex flex-col items-center justify-center">
             <Nav />
-            <div className="flex justify-center items-center flex-grow">
-                dashboard
+            <div className="flex flex-col flex-grow w-full">
+                <h1 className="text-2xl m-3">Dashboard</h1>
+                <div className="w-full">
+                    <h1 className="text-center mb-2">Registered Courses</h1>
+                    {
+                        registeredCourses.map(course => (
+                            <CourseCard
+                                key={course.code}
+                                name={course.name}
+                                code={course.code}
+                                location={course.location}
+                                handleClick={(e) => handleClick(e)}
+                            />
+                        ))
+                    }
+                </div>
+                <div className="w-full">
+                    <h1 className="text-center mt-10 mb-2">Available Courses</h1>
+                    {
+                        courses.map(course => (
+                            <CourseCard
+                                key={course.code}
+                                name={course.name}
+                                code={course.code}
+                                location={course.location}
+                                handleClick={(e: any) => handleClick(e)}
+                            />
+
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
