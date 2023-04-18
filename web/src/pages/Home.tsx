758389297660d1e664f87ee0745a55af7075f169
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-04-18 11:01:38 +0200

    improve  website responsivity

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -35,13 +35,13 @@ export const Home = () => {
 
     return <MainLayout>
             <div className="flex justify-center items-center">
-                <div className="flex flex-col md:flex-row h-full justify-center align-middle max-w-5xl p-12 gap-16">
-                    <div className="flex flex-col flex-1 align-middle justify-center gap-6 md:w-1/2 text-white">
+                <div className="flex flex-col sm:flex-row h-full justify-center align-middle max-w-5xl p-12 gap-16">
+                    <div className="flex flex-col flex-1 align-middle justify-center gap-6 sm:w-1/2 text-white">
                         <h1 className="font-bold text-5xl">Auto Attendance System</h1>
                         <p className="">This is main Way to register for the Auto Attendance System</p>
                         <button onClick={handleGetStarted} className="font-semibold text-lg p-2 bg-pink-600 rounded-full">Get Started!</button>
                     </div>
-                    <img src={IMG} className="md:w-1/2 flex-1 w-[1600px]" alt="Smart Camps Illustration" />
+                    <img src={IMG} className="sm:w-1/2 flex-1 w-[1600px]" alt="Smart Camps Illustration" />
                 </div>
             </div>
     </MainLayout>
