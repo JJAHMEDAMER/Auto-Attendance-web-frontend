4c30c2080dc649ec52c9e29e7bab0c706a58ea86
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-04-22 18:29:22 +0200

    add attendance section and center with position

--- web/src/comp/CourseCard.tsx
+++ web/src/comp/CourseCard.tsx
@@ -2,19 +2,26 @@ type courseCardPropType = {
     name: String
     code: String
     location: String
+    attendance?: String
     handleClick: React.MouseEventHandler<HTMLButtonElement>
     buttonText: String
 }
 
 
-export const CourseCard = ({ name, code, location, handleClick, buttonText }: courseCardPropType) => {
+export const CourseCard = ({ name, code, location, handleClick, buttonText, attendance: attendance }: courseCardPropType) => {
     return (
-        <div className="flex justify-between items-center md:w-2/3 md:max-w-2xl px-3 py-2 mb-1 border border-slate-900 rounded-lg bg-slate-800 mx-auto">
+        <div className="relative flex justify-between items-center md:w-2/3 md:max-w-2xl px-3 py-2 mb-1 border border-slate-900 rounded-lg bg-slate-800 mx-auto">
             <div>
                 <h2 className="text-xs">{code}</h2>
                 <h1 className="text-xl">{name}</h1>
                 <p className="text-pink-600 font-bold text-sm">Location: <span className="uppercase">{location}</span></p>
             </div>
+
+            {attendance
+                ? <div className="absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 text-gray-200 text-sm font-bold tracking-wider">Attendance:: <span className="text-green-500">{attendance}</span></div>
+                : null
+            }
+
             <button
                 data-code={code}
                 data-name={name}
