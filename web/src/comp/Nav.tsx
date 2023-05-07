c818cee476d7c9b450c93ed61b1e02a2a7c904e9
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-05-07 20:25:24 +0300

    add a hamburger menu for mobile view

--- web/src/comp/Nav.tsx
+++ web/src/comp/Nav.tsx
@@ -3,6 +3,9 @@ import ASULOGO from "../assets/asu-logo.png"
 import genericProfile from "../assets/genericProfile.png"
 import { myFetchGet } from "../utils/myFetch";
 
+//icons
+import { TbMenu2 } from "react-icons/tb"
+
 // NavLink 
 import { NavLink, useNavigate } from "react-router-dom";
 
@@ -11,6 +14,7 @@ import UserContext from "../utils/UserContext";
 
 export const Nav = () => {
     const [proPic, setProPic] = useState<string | null>("");
+    const [hidden, setHidden] = useState<boolean>(true)
     const { token, setToken } = useContext(UserContext)
 
     const navigate = useNavigate()
@@ -60,58 +64,75 @@ export const Nav = () => {
     ]
 
     return (
-        <nav className="fixed top-0 right-0 w-full z-50 text-gray-50 bg-slate-900 flex justify-between items-center py-2 lg:px-8 px-4 shadow">
-            <div className="flex gap-4 justify-center items-center">
-                <img src={ASULOGO} className="w-12 bg-gray-300 rounded-full" alt="ASU_LOGO" />
-                <p className="hidden lg:block text-lg">Auto Attendance System</p>
-            </div>
-            <ul className="flex gap-1 absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2">
+        <>
+            <nav className="fixed top-0 right-0 w-full z-50 text-gray-50 bg-slate-900 flex justify-between items-center py-2 lg:px-8 px-4 shadow">
+                <div className="flex gap-4 justify-center items-center">
+                    <TbMenu2 className="text-pink-600 md:hidden" size={30} onClick={() => { setHidden(!hidden) }} />
+                    <img src={ASULOGO} className="w-12 bg-gray-300 rounded-full" alt="ASU_LOGO" />
+                    <p className="hidden lg:block text-lg">Auto Attendance System</p>
+                </div>
+                <ul className="hidden md:flex gap-1 absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2">
+                    {
+                        navMenu.map((item, id) => (<li key={id}>
+                            <NavLink
+                                className="font-semibold px-3.5 py-1.5 rounded-full hover:bg-sky-600 hover:bg-opacity-10 hover:outline hover:outline-1 hover:outline-sky-600"
+                                to={item.link}
+                                style={({ isActive }) => isActive ? activeStyle : undefined}
+                            >
+                                {item.text}
+                            </NavLink>
+                        </li>))
+                    }
+                </ul>
+
+                <ul className="flex gap-1">
+                    {
+                        !token
+                            ? <>
+                                {
+                                    navLoginMenu.map((item, index) => (<li key={index}>
+                                        <NavLink
+                                            to={item.link}
+                                            className="font-semibold px-3.5 py-1.5 rounded-full hover:bg-sky-600"
+                                            style={({ isActive }) => isActive ? activeStyle : undefined}
+                                        >
+                                            {item.text}
+                                        </NavLink>
+                                    </li>
+                                    ))
+                                }
+                            </>
+                            : <>
+                                <img
+                                    src={proPic ? proPic : genericProfile}
+                                    className="w-10 h-10 object-cover rounded-full border-2 border-sky-600"
+                                    alt="genericProfile"
+                                />
+                                <button
+                                    onClick={logoutHandler}
+                                    className="font-semibold px-3.5 py-1.5 rounded-full hover:bg-sky-600"
+                                >
+                                    Log Out
+                                </button>
+                            </>
+                    }
+                </ul>
+
+            </nav>
+            <ul className="fixed top-16 bg-slate-900 w-full">
                 {
                     navMenu.map((item, id) => (<li key={id}>
                         <NavLink
-                            className="font-semibold px-3.5 py-1.5 rounded-full hover:bg-sky-600 hover:bg-opacity-10 hover:outline hover:outline-1 hover:outline-sky-600"
+                            className={`block font-semibold px-3.5 py-3 rounded-full ${hidden ? "hidden" : ""}`}
                             to={item.link}
                             style={({ isActive }) => isActive ? activeStyle : undefined}
                         >
                             {item.text}
                         </NavLink>
+                        {/* <br /> */}
                     </li>))
                 }
             </ul>
-
-            <ul className="flex gap-1">
-                {
-                    !token
-                        ? <>
-                            {
-                                navLoginMenu.map((item, index) => (<li key={index}>
-                                    <NavLink
-                                        to={item.link}
-                                        className="font-semibold px-3.5 py-1.5 rounded-full hover:bg-sky-600"
-                                        style={({ isActive }) => isActive ? activeStyle : undefined}
-                                    >
-                                        {item.text}
-                                    </NavLink>
-                                </li>
-                                ))
-                            }
-                        </>
-                        : <>
-                            <img
-                                src={proPic ? proPic : genericProfile}
-                                className="w-10 h-10 object-cover rounded-full border-2 border-sky-600"
-                                alt="genericProfile"
-                            />
-                            <button
-                                onClick={logoutHandler}
-                                className="font-semibold px-3.5 py-1.5 rounded-full hover:bg-sky-600"
-                            >
-                                Log Out
-                            </button>
-                        </>
-                }
-            </ul>
-
-        </nav>
+        </>
     )
 }
