ab9704a408f6cd001e0bd06ac72d5832f54cfaeb
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-05-12 05:50:32 +0300

    formate vite.config.ts && add host: true option

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-    //   '/api': {
-    //     target: 'https://localhost:8000',
-    //     changeOrigin: true,
-    //     secure: false,      
-    //     ws: true,
-    // }
- },
+  server: {
+    proxy: {
+      //   '/api': {
+      //     target: 'https://localhost:8000',
+      //     changeOrigin: true,
+      //     secure: false,
+      //     ws: true,
+      // }
+    },
     // port: 5000
+    // host: true  // run on local Network
   },
   plugins: [react()],
-})
+});
