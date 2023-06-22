7d566e5471eee092353b60acab047f534e06afbd
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-06-22 19:02:50 +0300

    make attendance responsive

--- web/src/pages/Attendance.tsx
+++ web/src/pages/Attendance.tsx
@@ -16,18 +16,18 @@ export const Attendance = () => {
     const atten = [
         { date: "12/12/12", attendance: "Attended" },
         { date: "12/12/12", attendance: "Attended" },
-        { date: "12/12/12", attendance: "Attended" },
+        { date: "12/12/12", attendance: "Absent" },
     ]
 
     if (!token) return  <Navigate to="/login" replace />;
     return (
         <MainLayout>
-            <div className='w-full px-5'>
+            <div className='w-full md:px-7 px-2'>
                 <h1 className='text-2xl m-3'>Attendance For {courseCode}</h1>
                 <div className='overflow-x-auto rounded-2xl'>
                     <table className='text-center w-full border-2 border-slate-900 rounded-2xl'>
                         <thead>
-                            <tr className='text-xl uppercase child:py-2 bg-slate-900'>
+                            <tr className='lg:text-xl text-lg uppercase child:py-2 bg-slate-900'>
                                 <th className='w-1/12'>#</th>
                                 <th className='w-5/12'>Date</th>
                                 <th className='w-6/12'>Attendance</th>
@@ -36,10 +36,10 @@ export const Attendance = () => {
                         <tbody className='[&>*:nth-child(even)]:bg-slate-700 child-hover:text-pink-600 child:transition-all'>
                             {
                                 atten.map((item, id) =>
-                                    <tr className='border-b border-slate-900'>
+                                    <tr className={`border-b border-slate-900 child:py-2`}>
                                         <td>{id}</td>
                                         <td>{item.date}</td>
-                                        <td>{item.attendance}</td>
+                                        <td className={`font-semibold ${item.attendance.toLowerCase() === "absent"? "text-red-700 ": "text-green-500"}`}>{item.attendance}</td>
                                     </tr>
                                 )
                             }
