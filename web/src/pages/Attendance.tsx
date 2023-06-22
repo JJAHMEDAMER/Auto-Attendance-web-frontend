fe7eed7268b8abde520ccc1c377c64eb636dfc88
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-06-22 18:42:35 +0300

    add main layout

--- web/src/pages/Attendance.tsx
+++ web/src/pages/Attendance.tsx
@@ -1,8 +1,14 @@
+import { MainLayout } from '../layout'
 import { useParams } from 'react-router-dom'
 
 export const Attendance = () => {
     const { courseCode } = useParams()
+
     return (
-        <div>Attendance: {courseCode}</div>
+        <MainLayout>
+            <div className='w-full px-5'>
+                <h1 className='text-2xl m-3'>Attendance For {courseCode}</h1>
+            </div>
+        </MainLayout>
     )
 }
