4eee5add1f77470d44cca971bd608b15c1049124
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-06-24 19:40:32 +0300

    update azure URL use custom dolman

--- web/src/utils/myFetch.ts
+++ web/src/utils/myFetch.ts
@@ -2,7 +2,7 @@
 // const BASEURL = "https://backend-auto-attendance.up.railway.app"  // Railway
 // const BASEURL = "http://127.0.0.1:8000";  // Local Dev
 // const BASEURL = "https://20.199.123.194";  // Microsoft Azure
-const BASEURL = "https://asu.francecentral.cloudapp.azure.com";  // Microsoft Azure
+const BASEURL = "https://api.ahmed-amer.tech";  // Microsoft Azure
 
 async function myFetchGet(url: string, token: string | null = null) {
   const URL = BASEURL + url;
